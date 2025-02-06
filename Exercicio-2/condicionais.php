7ec84e3a4568f0b42ce4ba2d3dbe5cb29a9386e1
Eduardo-Batist4 <carloswe3st@gmail.com>
2025-02-06 11:48:24 -0300

    new file to coditional

---
+++ Exercicio-2/condicionais.php
@@ -0,0 +1,84 @@
+<?php
+
+// --------------------- //
+// Exercicio 1
+// --------------------- //
+
+/* 
+$value = readline("Numero: ");
+
+if($value > 10) {
+    echo "É maior que 10! \n";
+} else {
+    echo "Não é maior que 10! \n";
+};
+*/
+
+// --------------------- //
+// Exercicio 2
+// --------------------- //
+
+/*
+$value = readline("Numero: ");
+
+if($value >= 0) {
+    echo "Positivo! \n";
+} else {
+    echo "Negativo! \n";
+};
+*/
+
+// --------------------- //
+// Exercicio 3
+// --------------------- //
+
+/*
+$appleQuantity = readline("Quantas maçãs: \n");
+
+if($appleQuantity < 12) {
+    $result = number_format($appleQuantity * 1.30, 2);
+    echo "Valor Total: R$: $result \n";
+} else {
+    $result = number_format($appleQuantity * 1.00, 2);
+echo "Valor Total: R$: $result \n";
+};
+*/
+
+// --------------------- //
+// Exercicio 4
+// --------------------- //
+
+/*
+$firstGrade = readline("Primeira Nota: \n");
+$secondGrade = readline("Segunda Nota: \n");
+
+$average = ($firstGrade + $secondGrade) / 2;
+
+if($average >= 60) {
+    echo "Aluno foi Aprovado! Média: $average \n";
+} else {
+    echo "Aluno foi Reprovado! Média: $average \n";
+};
+*/
+
+// --------------------- //
+// Exercicio 5
+// --------------------- //
+
+/*
+$yearOfBirth = readline("Ano de Nascimento: \n");
+
+$calculateAge = 2025 - $yearOfBirth;
+
+if($calculateAge < 16) {
+    echo "Sua idade: $calculateAge \n Não pode votar!";
+} else if ($calculateAge >= 16 && $calculateAge < 18) {
+    echo "Sua idade: $calculateAge \n Voce pode votar! \n Não pode tirar a carteira! \n";
+} else {
+    echo "Sua idade: $calculateAge \n Voce pode votar! \n Voce pode tirar a carteira! \n";
+}
+*/
+
+// --------------------- //
+// Exercicio 6
+// --------------------- //
