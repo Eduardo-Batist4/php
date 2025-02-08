c75821c65bcc69e8d216dc66bc4665a62271811e
Eduardo Batista <carloswe3st@gmail.com>
2025-02-08 15:59:29 -0300

    exercises 1 and 2 completed

--- Exercicio-1/conceito_basico.php
+++ Exercicio-1/conceito_basico.php
@@ -17,12 +17,12 @@ $result = $input_1 / $input_2;
 $reminderDivision = $input_1 % $input_2;
 
 echo "Resultado: $result Resto da divisão: $reminderDivision \n";
-*/
+
 
 // ---------------------- //
 // Exercicio B 
 // ---------------------- //
-/* 
+
 echo 'First grade: ';
 $input_1 = readline();
 $weight1 = 1;
@@ -46,12 +46,12 @@ echo "\n";
 $calculate = (($input_1 * $weight1) + ($input_2 * $weight2) + ($input_3 * $weight3) + ($input_4 * $weight4)) / ($weight1 + $weight2 + $weight3 + $weight4);
 
 echo "Resultado: " . number_format($calculate, 2);
-*/
+
 
 // ---------------------- //
 // Exercicio C 
 // ---------------------- //
-/* 
+
 echo 'Value product: ';
 $valueProduct = readline();
 
@@ -61,12 +61,12 @@ $percent = readline();
 $newValue = $valueProduct * ( 1 + ($percent / 100));
 
 echo "Novo preço: " . number_format($newValue, 2);
-*/
+
 
 // ---------------------- //
 // Exercicio D
 // ---------------------- //
-/* 
+
 echo 'Salary: ';
 $salary = readline();
 
@@ -81,13 +81,13 @@ echo "Valor de cada quilowatt: R$:" . number_format($valueQuilowatt, 2) . "\n";
 echo "Valor: R$:" . number_format($amountToBePaid) . "\n";
 echo "Valor com Desconto de 10%: R$:" .  number_format($discountedPrice) . "\n";
 
-*/
+
 
 // ---------------------- //
 // Exercicio E
 // ---------------------- //
 
-/*
+
 $base = readline("Base: \n");
 $height = readline("Altura: \n");
 
@@ -98,39 +98,39 @@ $diagonal = number_format(sqrt(($base ** 2) + ($height ** 2)), 2);
 echo "Perímetro: $perimeter \n";
 echo "Area: $area \n";
 echo "Diagonal: $diagonal \n";
-*/
+
 
 // ---------------------- //
 // Exercicio F
 // ---------------------- //
 
-/*
+
 $base = readline("Base: \n");
 $height = readline("Altura: \n");
 
 $result = ($base * $height) / 2;
 
 echo "Area do triangulo: $result \n";
-*/
+
 
 // ---------------------- //
 // Exercicio G
 // ---------------------- //
 
-/*
+
 $longestDiagonal = readline("Diagonal maior: \n");
 $smallerDiagonal = readline("Diagonal menor: \n");
 
 $result = ($longestDiagonal * $smallerDiagonal) / 2;
 
 echo "Area do losango: $result \n";
-*/
+
 
 // ---------------------- //
 // Exercicio I
 // ---------------------- //
 
-/* 
+
 $fixedSalary = readline("Salario fixo: \n");
 $numberOfCars = readline("Numeros de carros vendidos: \n");
 $commissionValue = readline("Valor da comissao: \n");
@@ -139,4 +139,50 @@ $totalSalesValue = readline("Valor total de vendas: \n");
 $finalSalary = $fixedSalary + ($numberOfCars * $commissionValue) + (0.05 * $totalSalesValue);
 
 echo "Salario final: $finalSalary";
+
+
+// ---------------------- //
+// Opcional J
+// ---------------------- //
+
+
+$hour = readline("Horas: \n");
+
+$minutes = $hour * 60;
+
+echo "$minutes minutos se passaram desde o inicio do dia.";
+
+
+// ---------------------- //
+// Opcional K
+// ---------------------- //
+
+
+$numberCDU = readline("Digite um numero no formato CDU: \n");
+
+$reversed = strrev($numberCDU);
+
+echo "$reversed \n";
+
+// ---------------------- //
+// Opcional K
+// ---------------------- //
+
+$number = readline("Digite um numero : \n");
+
+$reversed = strrev($number);
+$sum = $number + $reversed;
+
+echo "$sum \n";
+$hundred = (int) ($sum / 100);
+$ten = (int) (($sum % 100) / 10);
+$unit = (int) ($sum % 10);
+
+
+$result = ($hundred * 1) + ($ten * 2) + ($unit * 3);
+
+
+$verifyDigit = $result % 10;
+
+echo "Digito verificador: $verifyDigit \n";
 */
--- Exercicio-2/condicionais.php
+++ Exercicio-2/condicionais.php
@@ -12,13 +12,13 @@ if($value > 10) {
 } else {
     echo "Não é maior que 10! \n";
 };
-*/
+
 
 // --------------------- //
 // Exercicio 2
 // --------------------- //
 
-/*
+
 $value = readline("Numero: ");
 
 if($value >= 0) {
@@ -26,13 +26,13 @@ if($value >= 0) {
 } else {
     echo "Negativo! \n";
 };
-*/
+
 
 // --------------------- //
 // Exercicio 3
 // --------------------- //
 
-/*
+
 $appleQuantity = readline("Quantas maçãs: \n");
 
 if($appleQuantity < 12) {
@@ -42,13 +42,13 @@ if($appleQuantity < 12) {
     $result = number_format($appleQuantity * 1.00, 2);
 echo "Valor Total: R$: $result \n";
 };
-*/
+
 
 // --------------------- //
 // Exercicio 4
 // --------------------- //
 
-/*
+
 $firstGrade = readline("Primeira Nota: \n");
 $secondGrade = readline("Segunda Nota: \n");
 
@@ -59,13 +59,13 @@ if($average >= 60) {
 } else {
     echo "Aluno foi Reprovado! Média: $average \n";
 };
-*/
+
 
 // --------------------- //
 // Exercicio 5
 // --------------------- //
 
-/*
+
 $yearOfBirth = readline("Ano de Nascimento: \n");
 
 $calculateAge = 2025 - $yearOfBirth;
@@ -77,13 +77,13 @@ if($calculateAge < 16) {
 } else {
     echo "Sua idade: $calculateAge \n Voce pode votar! \n Voce pode tirar a carteira! \n";
 }
-*/
+
 
 // --------------------- //
 // Exercicio 6
 // --------------------- //
 
-/*
+
 $firstValue = readline("Primeiro Numero: \n");
 $secondValue = readline("Segundo Numero: \n");
 
@@ -92,13 +92,13 @@ if($firstValue > $secondValue) {
 } else {
     echo "O $secondValue é maior do que $firstValue \n";
 };
-*/
+
 
 // --------------------- //
 // Exercicio 7
 // --------------------- //
 
-/*
+
 $swimmerAge = readline("Idade do nadador: \n");
 
 if($swimmerAge >= 5 && $swimmerAge <= 7) {
@@ -114,13 +114,13 @@ if($swimmerAge >= 5 && $swimmerAge <= 7) {
 } else {
     echo "O nadador não tem categoria \n";
 };
-*/
+
 
 // --------------------- //
 // Exercicio 8
 // --------------------- //
 
-/*
+
 $productValue = readline("Valor produto: \n");
 
 $cashWith10Discount = $valueProduct - ($valueProduct * 0.1);
@@ -132,13 +132,13 @@ echo "Á vista em dinheiro com 10% de desconto: $cashWith10Discount \n";
 echo "Á vista no cartão com 5% de desconto: $cardWith5Discount \n";
 echo "Em 2 vezes, preço normal da etiqueta: $regular \n";
 echo "Em 3 vezes, preço normal da etiqueta com juros de 10%: $withInterest10 \n";
-*/
+
 
 // --------------------- //
 // Exercicio 9
 // --------------------- //
 
-/*
+
 $number = readline("Digite um numero: \n");
 
 if($number >= 30 && $number <= 90) {
@@ -148,13 +148,13 @@ if($number >= 30 && $number <= 90) {
 } else {
     echo "O numero $number não se encaixa \n";
 }
-*/
+
 
 // --------------------- //
 // Exercicio 10
 // --------------------- //
 
-/*
+
 $productValue = readline("Valor do produto: \n");
 
 $profit45Percent = $productValue + ($productValue * 0.45);
@@ -165,8 +165,112 @@ if($productValue < 20) {
 } else {
     echo "Margem de lucro de 30%: R$:$profit30Percent \n";
 };
-*/
 
 // --------------------- //
 // OPCIONAL 11
-// --------------------- /
+// --------------------- //
+
+// Não entendi
+
+// --------------------- //
+// OPCIONAL 12
+// --------------------- //
+
+$value1 = readline("Primeiro Numero: \n");
+$value2 = readline("Segundo Numero: \n");
+$value3 = readline("Terceiro Numero: \n");
+$value4 = readline("Quarto Numero: \n");
+
+$pair = PHP_INT_MAX;
+
+if($value1 % 2 == 0) {
+    if($value1 < $pair) {
+        $pair = $value1;
+        echo "Menor valor par: $pair \n";
+    }
+} else if ($value2 % 2 == 0) {
+    if($value2 < $pair) {
+        $pair = $value2;
+        echo "Menor valor par: $pair \n";
+    }
+} else if ($value3 % 2 == 0) {
+    if($value3 < $pair) {
+        $pair = $value3;
+        echo "Menor valor par: $pair \n";
+    }
+} else if ($value4 % 2 == 0) {
+    if($value4 < $pair) {
+        $pair = $value4;
+        echo "Menor valor par: $pair \n";
+    }
+} else {
+    echo "Nenhum valor é par! \n";
+}
+
+// --------------------- //
+// OPCIONAL 13
+// --------------------- //
+
+$startTimeOfTheGame = readline("Horário de inicio: \n");
+$endTimeOfTheGame = readline("Horário final: \n");
+
+$calculateHour = $endTimeOfTheGame - $startTimeOfTheGame;
+
+if($endTimeOfTheGame < $startTimeOfTheGame) {
+    $result = (24 - $startTimeOfTheGame) + $endTimeOfTheGame;
+    echo "A duração do jogo foi de: $result horas \n";
+} else {
+    echo "A duração do jogo foi de: $calculateHour horas \n";
+};
+
+// --------------------- //
+// OPCIONAL 14
+// --------------------- //
+
+$hoursWorked = readline("Horas trabalhadas: \n");
+$hourlyWage = readline("Salario por hora: \n");
+
+$regularMonthlyHours = 40 * 4;
+$basicSalary = $regularMonthlyHours * $hourlyWage;
+
+if($hoursWorked > $regularMonthlyHours) {
+    $overtime = $hoursWorked - $regularMonthlyHours;
+    $overtimeValue = $hourlyWage + ($hourlyWage * 0.5);
+    $totalSalary = $basicSalary + ($overtime * $overtimeValue);
+    
+    echo "\nSalário base: R$:$basicSalary \n";
+    echo "Horas extra: $overtime horas\n";
+    echo "Valor da horas extra: R$:$overtimeValue \n";
+    echo "Salário total: R$:$totalSalary \n";
+} else if ($hoursWorked == 160) {
+    $basicSalary = $hoursWorked * $hourlyWage;
+    echo "\nSalário base: R$:$basicSalary \n";
+    echo "Horas extra: 0 horas\n";
+} else {
+    $hourlyWageLess = $hoursWorked * $hourlyWage;
+$discount5Percent = $hourlyWageLess * 0.05;
+$discountedSalary = $hourlyWageLess - $discount5Percent;
+
+echo "\nSalário base: R$:$hourlyWageLess \n";
+echo "Horas extra: 0 horas\n";
+echo "Desconto de: R$:$discount5Percent \n";
+echo "Salário total: R$:$discountedSalary \n";
+}
+
+// --------------------- //
+// OPCIONAL 15
+// --------------------- //
+
+$fixedSalary = readline("Salário fixo: \n");
+$salesValue = readline("Valor das vendas: \n");
+
+
+if($salesValue <= 1500) {
+    $commisionOf3Percent = $fixedSalary + ($salesValue * 0.03);
+    echo "$commisionOf3Percent \n";
+} else {
+    $commisionOf5Percent = 1500 * 0.03 + (($salesValue - 1500) * 0.05);
+$totalSalary = $fixedSalary + $commisionOf5Percent;
+echo "$totalSalary \n";
+}
+*/
