2f38829b174d35eeadf589dbf8ba2e2bbd8bd779
Eduardo-Batist4 <carloswe3st@gmail.com>
2025-02-10 08:26:34 -0300

    fix ecercise 8 and finish exercise 10

--- Exercicio-3/loops.php
+++ Exercicio-3/loops.php
@@ -8,13 +8,13 @@
 for ($i = 1; $i < 11; $i++) {
     echo "$i \n";
 };
-*/
+
 
 // --------------------- //
 // Exercicio 2
 // --------------------- //
 
-/*
+
 $media = 0;
 
 for ($i = 1; $i < 11; $i++) {
@@ -25,13 +25,12 @@ for ($i = 1; $i < 11; $i++) {
 $resultMedia = $media / 10;
 
 echo "A media é: $resultMedia \n";
-*/
+
 
 // --------------------- //
 // Exercicio 3
 // --------------------- //
 
-/*
 $count = 0;
 $sumNums = 0;
 
@@ -49,13 +48,13 @@ while(true) {
     exit;
 };
 };
-*/
+
 
 // --------------------- //
 // Exercicio 4
 // --------------------- //
 
-/*
+
 $count = 0;
 $sum = 0;
 
@@ -69,13 +68,13 @@ $media = $sum / $count;
 
 echo "Somatoria: $sum \n";
 echo "Média: $media \n";
-*/
+
 
 // --------------------- //
 // Exercicio 5
 // --------------------- //
 
-/*
+
 $smaller = PHP_INT_MAX;
 $larger = PHP_INT_MIN;
 
@@ -95,13 +94,13 @@ while(true) {
     exit;
 }
 };
-*/
+
 
 // --------------------- //
 // OPCIONAL 6
 // --------------------- //
 
-/* 
+ 
 while(true) {
 
     $firstGrade = readline("Primeira nota: \n");
@@ -126,13 +125,13 @@ while(true) {
         echo "Nota inválida! (0 a 10). \n";
     };
 };
-*/
+
 
 // --------------------- //
 // OPCIONAL 7
 // --------------------- //
 
-/* 
+ 
 $quantityOfNegativeNumbers = 0;
 
 for ($i = 0; $i < 10; $i++) {
@@ -143,13 +142,12 @@ for ($i = 0; $i < 10; $i++) {
 };
 
 echo "Quantidade de numeros negativos: $quantityOfNegativeNumbers \n";
-*/
 
 // --------------------- //
 // OPCIONAL 8
 // --------------------- //
 
-/* 
+
 $totalValue = 0;
 $totalMerchandise = 0;
 
@@ -177,4 +175,39 @@ while (true) {
         };
     };
 };
+
+
+// --------------------- //
+// OPCIONAL 9
+// --------------------- //
+
+$totalValue = 0;
+$totalMerchandise = 0;
+
+while (true) {
+
+    $valueProduct = readline("Valor da mercadoria:  \n");
+    $totalMerchandise++;
+    $totalValue += $valueProduct;
+    $more = readline("Mais mercadorias (S/N)? \n");
+    system('clear');
+
+    if($more == 's' || $more == 'S') {
+        continue;
+    } else {
+        $calculateAverage = number_format($totalValue / $totalMerchandise, 2);
+        echo "Valor total do estoque: R$:$totalValue \n";
+        echo "Média do valor das mercadorias: R$:$calculateAverage \n";
+        exit;
+    };
+};
 */
+
+// --------------------- //
+// OPCIONAL 10
+// --------------------- //
+
+
+
+
+
